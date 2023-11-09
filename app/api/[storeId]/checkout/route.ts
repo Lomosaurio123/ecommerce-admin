053d7886a0ba642b17685644ec091e2dace5a4e2
Lomosaurio123 <mhernandezl1605@alumno.ipn.mx>
2023-11-08 21:57:27 -0600

    cors

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,6 +1,4 @@
-
 import { NextResponse } from "next/server";
-
 import prismadb from "@/lib/prismadb";
 
 const corsHeaders = {
@@ -17,61 +15,52 @@ export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
-    try {
+  try {
+    const { productIds, phone, address, totalPrice } = await req.json();
 
-      const { productIds, phone, address, totalPrice } = await req.json();
-
-      if (!phone) {
-        return new NextResponse("Phone is required", { status: 403 });
-      }
-
-      if (!address) {
-        return new NextResponse("Address is required", { status: 403 });
-      }
-
-      if (!totalPrice) {
-        return new NextResponse("Total price is required", { status: 403 });
-      }
-
-      if (!productIds || productIds.length === 0) {
-        return new NextResponse("Product ids are required", { status: 400 });
-      }
-    
-      const products = await prismadb.product.findMany({
-        where: {
-          id: {
-            in: productIds
-          }
-        }
-      });
-  
-    
-      const order = await prismadb.order.create({
-        data: {
-          phone,
-          address,
-          totalPrice,
-          storeId: params.storeId,
-          isPaid: false,
-          orderItems: {
-            create: productIds.map((productId: string) => ({
-              product: {
-                connect: {
-                  id: productId
-                }
-              }
-            }))
-          }
-        }
-      });
-
-      return NextResponse.json( order , {
-        headers: corsHeaders
-      } )
-      
-    } catch (error) {
-      console.log('[CHECKOUT_POST]', error);
-      return new NextResponse("Internal error", { status: 500 });
+    if (!phone) {
+      return new NextResponse("Phone is required", { status: 403 });
     }
 
-};
+    if (!address) {
+      return new NextResponse("Address is required", { status: 403 });
+    }
+
+    if (!totalPrice) {
+      return new NextResponse("Total price is required", { status: 403 });
+    }
+
+    if (!productIds || productIds.length === 0) {
+      return new NextResponse("Product ids are required", { status: 400 });
+    }
+
+    const order = await prismadb.order.create({
+      data: {
+        phone,
+        address,
+        totalPrice,
+        storeId: params.storeId,
+        isPaid: false,
+        orderItems: {
+          create: productIds.map((productId: string) => ({
+            product: {
+              connect: {
+                id: productId
+              }
+            }
+          }))
+        }
+      }
+    });
+
+    return NextResponse.json(order, {
+      headers: corsHeaders,
+      status: 200
+    });
+
+  } catch (error) {
+    console.log('[CHECKOUT_POST]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
