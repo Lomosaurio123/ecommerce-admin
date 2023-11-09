560f9dbdce1861f3df2d0cb1e20f03e30013bc88
Lomosaurio123 <mhernandezl1605@alumno.ipn.mx>
2023-11-08 23:43:33 -0600

    solucionando error

---
+++ app/api/[storeId]/orders/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+
+import prismadb from '@/lib/prismadb';
+
+export async function GET(
+    req: Request,
+  ) {
+    try {
+
+        const { phone } = await req.json();
+
+        if (!phone) {
+            return new NextResponse("Phone is required", { status: 400 });
+        }
+    
+        const orders = await prismadb.order.findMany({
+            where: {
+            phone: phone
+            }
+        });
+        
+        return NextResponse.json(orders);
+    } catch (error) {
+      console.log('[ORDERS_GET]', error);
+      return new NextResponse("Internal error", { status: 500 });
+    }
+  };
